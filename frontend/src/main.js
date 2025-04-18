0ab6b70ac9c05bf5235a0cc1b267ab946e497bdb
vebjornkjus <vebjornfk@uia.no>
2025-04-18 16:42:09 +0200

    Map Update and data update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
-const map = L.map('map').setView([58.14671, 7.9956], 12); // Kristiansand
+// Coordinates for Norway
+const map = L.map('map').setView([61.14671, 9.9956], 6); // Norway
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
